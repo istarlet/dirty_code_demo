eedcc385bdd9e58e64ce40c9c2e60c5300f31820
istarlet <istarlet@gmail.com>
2022-10-04 14:03:45 -0400

    No idea what I'm doing

--- users.py
+++ users.py
@@ -32,33 +32,40 @@
 
 # A program to take input of user names, birthdays and favorite hobby and writes them to a file
 
-file = open("users.csv", mode="w")
+# file = open("users.csv", mode="w")
 
-last_input = ""
+# last_input = ""
 
-while last_input != "Quit":
+# while last_input != "Quit":
 
-    # add a header to the file
-    file.write("first_name, last_name, birth_year, favorite_hobby\n")
+#     # add a header to the file
+#     file.write("first_name, last_name, birth_year, favorite_hobby\n")
 
-    print("Please input user's first name. 'Quit' to exit the program")
-    last_input = input()
-    first = last_input
+#     print("Please input user's first name. 'Quit' to exit the program")
+#     last_input = input()
+#     first_name = last_input
 
-    print("Please input users last name 'Quit' to exit the program")
-    last_input = input()
-    second = last_input
+#     print("The first name you entered is:", first_name)
 
-    print("Please input users birthday_year. 'Quit' to exit the program")
-    last_input = str(input())
-    third = last_input
+#     print("Please input users last name 'Quit' to exit the program")
+#     last_input = input()
+#     last_name = last_input
+
+# # while True:
+#     # try:
+#     print("Please input users birth year. 'Quit' to exit the program")
+#     last_input = int(input())
+#     birth_year = last_input
     
-    print("Please input users favorite hobby. 'Quit' to exit the program")
-    last_input = input()
-    fourth = last_input
+#     # except ValueError:
+#     #     print("Invalid Input. Expected a number. Please try again.")
+    
+#     print("Please input users favorite hobby. 'Quit' to exit the program")
+#     last_input = input()
+#     favorite_hobby = last_input
 
-    #use a format string to write an input to our file
-    file.write(first + "," + second + "," + third + "," + fourth)
+#     #use a format string to write an input to our file
+#     file.write(first_name + "," + last_name + "," + birth_year + "," + favorite_hobby)
 
-file.close()
+# file.close()
 
